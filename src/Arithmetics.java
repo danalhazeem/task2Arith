e4f478875509e14867a5e5b5ba9111ea7c0ea84b
danalhazeem <92695924+danalhazeem@users.noreply.github.com>
2024-11-03 11:32:12 +0300

    task

---
+++ src/Arithmetics.java
@@ -0,0 +1,15 @@
+class Arithmetics {
+    public static void main(String[] args) {
+        int amountKWD = 86;
+        double kuwaitiDinarToUSDollars =  amountKWD * 3.25;
+        System.out.println(amountKWD+" KWD = "+kuwaitiDinarToUSDollars+" USD");
+
+        double amountUs = 279.5 ;
+        double USDollarsToKuwaitiDinar =  amountUs / 3.25;
+        System.out.println(amountUs+" USD = "+USDollarsToKuwaitiDinar+" KWD");
+
+        int amountEUR = 1000 ;
+        double EURToKuwaitiDinar =  amountEUR * 0.332;
+        System.out.println(amountEUR+" EUR = "+EURToKuwaitiDinar+" KWD");
+    }
+}
